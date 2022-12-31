fdd4b0475b7dd917f59196c8fb22a0534bfc544d
itsmarsss <itsmarzzzzzz@gmail.com>
2022-12-31 00:22:40 -0500

    Fix display text

--- src/macro/MacroForm.java
+++ src/macro/MacroForm.java
@@ -70,7 +70,7 @@ public class MacroForm {
                 if(macrosList.getSelectedIndex() == 0){
                     System.out.println(macrosList.getSelectedIndex());
                     KillKeyEdit kke = new KillKeyEdit();
-                    JFrame editFrame = new JFrame("Editing macro: \"" + selected + "\"");
+                    JFrame editFrame = new JFrame("Editing macro: \"KILLKEY");
                     editFrame.setContentPane(kke.editPanel);
                     kke.edit(selected, editFrame, frame);
                     editFrame.pack();
